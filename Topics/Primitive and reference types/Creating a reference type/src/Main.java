0c3c90d9e412e0ea2f53de4ca6b1eff2b86e9d6a
Marco Au@Toronto <cau28@myseneca.ca>
2023-01-06 23:40:55 -0500

    Delete Topics directory

--- Topics/Primitive and reference types/Creating a reference type/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // put your code here
-        String hello = new String("Hello, learner!");
-        System.out.print(hello);
-    }
-}
